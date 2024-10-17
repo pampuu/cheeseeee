22c48a665c5854d52217801e0f693ecbe2588709
Rashim Bhandari <122297196+pampuu@users.noreply.github.com>
2024-10-18 00:15:43 +0545

    Add files via upload

---
+++ cheese/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="footer-content">
+        <p>&copy; 2024 The CheeseSteak Factory. All Rights Reserved.</p>
+        <p>Contact us at <a href="mailto:info@cheesefactory.com">info@cheesefactory.com</a></p>
+        <div class="social-links">
+            <p>Social Media at <a href="https://www.instagram.com/_thecheesesteakfactory/?hl=en" target="_blank">
+                <img src="instagram.png" alt="Instagram" style="width: 30px; height: 25px; margin-top: 10px;">
+            </a></p>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
